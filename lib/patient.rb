36bda26d81d864774ffc3c2c4aa0491e125caa27
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-04 22:20:07 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,12 +1,10 @@
 class Patient
   attr_accessor :name, :appointments, :doctors
-  @@all = []
 
   def initialize(name)
     @name = name
     @appointments = []
     @doctors =[]
-    @@all << self
   end
 
   def self.all
