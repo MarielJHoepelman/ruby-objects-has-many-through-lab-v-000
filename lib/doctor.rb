f2666314f3832da60618bfa5ead180d911d4005c
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-04 22:00:35 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,30 +1,30 @@
-class Artist
-  attr_accessor :name, :genres
-  @@all = []
+class Doctor
+  attr_accessor :name
+  #@@all = []
 
   def initialize(name)
     @name = name
-    @genres = []
-    @songs = []
-    @@all << self
+    #@genres = []
+    #@songs = []
+    #@@all << self
   end
-
-  def self.all
-    @@all
-  end
-
-  def new_song(name, genre)
-    song = Song.new(name, self, genre)
-    @songs << song
-    @genres << genre
-    song
-  end
-
-  def songs
-    @songs
-  end
-
-  def genres
-    @genres
-  end
+  #
+  # def self.all
+  #   @@all
+  # end
+  #
+  # def new_song(name, genre)
+  #   song = Song.new(name, self, genre)
+  #   @songs << song
+  #   @genres << genre
+  #   song
+  # end
+  #
+  # def songs
+  #   @songs
+  # end
+  #
+  # def genres
+  #   @genres
+  # end
 end
