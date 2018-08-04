7410ed4971efffb9f811596fe71c541769588433
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-04 22:01:09 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,10 +8,10 @@ class Doctor
     #@songs = []
     @@all << self
   end
-  #
-  # def self.all
-  #   @@all
-  # end
+
+  def self.all
+    @@all
+  end
   #
   # def new_song(name, genre)
   #   song = Song.new(name, self, genre)
