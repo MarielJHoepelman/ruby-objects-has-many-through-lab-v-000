fda90e0731a179779b36dbcdba4bb00b4084476c
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-04 21:44:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,15 @@
 class Song
   attr_accessor :name, :artist, :genre
-  #@@all = []
+  @@all = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist  = artist
     @genre = genre
   end
+  
+  def self.all
+    @@all
+  end
+
 end
