77b025c03b6da5ed257e83748f65c7a11fdc16aa
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-04 22:10:49 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
   def self.all
     @@all
   end
-  #
+  
   def new_appointment(patient, date)
     appointment = Appointment.new(patient, self, date)
     @appointments << appointment
