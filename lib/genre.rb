54fa8031a5a634f04ffa77a4d6ace5a46b22d757
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-04 21:59:30 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,6 +1,5 @@
 class Genre
   attr_accessor :name, :songs, :artists
-  #@@all = []
 
   def initialize(name)
     @name = name
@@ -15,5 +14,4 @@ class Genre
   def artists
     @artists
   end
-
 end
